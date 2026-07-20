23225edc314e2f4e0f7236e99e04da88b056c2e7
SantiJ10 <99493595+SantiJ10@users.noreply.github.com>
2026-07-19 23:21:45 -0300

    Cliente PvP Modern 0.6.12: hotfix crash MixinCameraFreelook al iniciar.
    Camera.update en 1.21.11 usa World como primer parametro, no BlockView.

--- client-modern/src/main/java/com/paraguacraft/pvp/modern/mixin/MixinCameraFreelook.java
+++ client-modern/src/main/java/com/paraguacraft/pvp/modern/mixin/MixinCameraFreelook.java
@@ -3,7 +3,7 @@ package com.paraguacraft.pvp.modern.mixin;
 import com.paraguacraft.pvp.modern.core.FreelookManager;
 import net.minecraft.client.render.Camera;
 import net.minecraft.entity.Entity;
-import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,7 +18,7 @@ public abstract class MixinCameraFreelook {
 
     @Inject(method = "update", at = @At("RETURN"))
     private void paraguacraft$freelook(
-        BlockView area,
+        World area,
         Entity focusedEntity,
         boolean thirdPerson,
         boolean inverseView,
